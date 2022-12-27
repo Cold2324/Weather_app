8d634f190c60d9afc12d55855e1e393d6b77db8a
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-12-27 03:22:51 -0500

    images added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import {useEffect} from 'react'
-import getWeather from './api/getWeather'
+import { useEffect, useState } from "react";
+import getWeather from "./api/getWeather";
 export default function App() {
-  useEffect(() => {
-    getWeather("904d2b0ae7cb4f3a83674209222712", "Lima")
-      .then(res => console.log(res))
-  }, [])
+  const [city, setCity] = useState("");
+  const [onSubmit, setOnSubmit] = useState(false);
 
+  useEffect(() => {
+    getWeather("904d2b0ae7cb4f3a83674209222712", city)
+      .then((res) => console.log(res))
+      .catch((e) => console.log(e));
+  }, [onSubmit]);
+
+  const handleChangeInput = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setOnSubmit(true);
+  };
 
   return (
-    <div >
-      Hello World
+    <div className="w-[100%] h-screen bg-[url('./assets/sun.jpg')]">
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChangeInput}
+          className="outline-none p-5"
+          type="text"
+          placeholder="Lima"
+        />
+      </form>
     </div>
-  )
+  );
 }
