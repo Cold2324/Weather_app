e3685eb103f3cd0dd6d3a122cde31f6abb1e69e9
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-12-27 11:22:53 -0500

    methods to manage the submit and change

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useEffect, useState } from "react";
-import getWeather from "./api/getWeather";
+import { useEffect, useState } from 'react'
+import getWeather from './api/getWeather'
 export default function App() {
-  const [city, setCity] = useState("");
-  const [onSubmit, setOnSubmit] = useState(false);
+  const [city, setCity] = useState('')
+  const [onSubmit, setOnSubmit] = useState(false)
+  const [resJSON, setResJSON] = useState(undefined)
 
   useEffect(() => {
-    getWeather("904d2b0ae7cb4f3a83674209222712", city)
-      .then((res) => console.log(res))
-      .catch((e) => console.log(e));
-  }, [onSubmit]);
+    getWeather(city)
+      .then((res) => setResJSON(res))
+      .catch((e) => console.log(e))
+  }, [onSubmit])
 
   const handleChangeInput = (e) => {
-    setCity(e.target.value);
-  };
+    setCity(e.target.value)
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setOnSubmit(true);
-  };
+    e.preventDefault()
+    setOnSubmit(true)
+  }
 
   return (
-    <div className="w-[100%] h-screen bg-[url('./assets/sun.jpg')]">
-      <form onSubmit={handleSubmit}>
-        <input
-          onChange={handleChangeInput}
-          className="outline-none p-5"
-          type="text"
-          placeholder="Lima"
-        />
-      </form>
+    <div className="w-[100%] h-screen bg-[url('./assets/cold.jpg')]">
+      <section>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChangeInput}
+            className="outline-none p-5 bg-transparent"
+            type="text"
+            placeholder="Lima"
+          />
+        </form>
+      </section>
     </div>
-  );
+  )
 }
--- src/api/getWeather.js
+++ src/api/getWeather.js
@@ -1,4 +1,5 @@
-export default async function getWeather(API_KEY, CITY) {
+export default async function getWeather(CITY) {
+  const API_KEY = `${import.meta.env.VITE_API_KEY}`
   const URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${CITY}&aqi=yes`
   const res = await fetch(URL)
   const resJSON = await res.json()
