a83d60fc2b8bbd128b0915b87e0cb36534080218
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-12-31 00:54:19 -0500

    add responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
+import { cold, hot } from './utils/theme'
 import getWeather from './api/getWeather'
+
 export default function App() {
   const [city, setCity] = useState('Lima')
   const [onSubmit, setOnSubmit] = useState(false)
@@ -22,7 +24,17 @@ export default function App() {
   }
 
   return (
-    <div className="w-[100%] h-screen bg-[url('./assets/cold.jpg')]">
+    <div
+      className={`w-[100%] h-screen ${
+        parseFloat(resJSON?.current?.temp_c) > 15
+          ? hot.background
+          : cold.background
+      } ${
+        parseFloat(resJSON?.current?.temp_c) > 15
+          ? hot.dBackground
+          : cold.dBackground
+      }`}
+    >
       <section className="p-5">
         <form onSubmit={handleSubmit}>
           <input
@@ -35,15 +47,25 @@ export default function App() {
         </form>
       </section>
       <section className="p-5 flex flex-col items-center justify-center">
-        <p className="text-bold text-[70px] text-gray-700">
+        <p
+          className={`text-bold text-[70px] text-gray-700 ${
+            parseFloat(resJSON?.current?.temp_c) < 15 && 'md:text-white'
+          }`}
+        >
           {resJSON?.location?.name}
         </p>
         {resJSON.hasOwnProperty('error') ? (
-          <p className="pl-3 text-bold text-[30px] text-gray-700">
+          <p
+            className={`pl-3 text-bold text-[30px] text-gray-700 md:text-white`}
+          >
             {resJSON?.error?.message}
           </p>
         ) : (
-          <p className="pl-5 text-bold text-[60px] text-gray-700">
+          <p
+            className={`pl-5 text-bold text-[60px] text-gray-700 ${
+              parseFloat(resJSON?.current?.temp_c) < 15 && 'md:text-white'
+            }`}
+          >
             {resJSON?.current?.temp_c}°
           </p>
         )}
---
+++ src/utils/theme.js
@@ -0,0 +1,11 @@
+const cold = {
+  background: "bg-[url('./assets/cold.jpg')]",
+  dBackground: "md:bg-[url('./assets/coldDesk.jpg')]",
+}
+
+const hot = {
+  background: "bg-[url('./assets/sun.jpg')]",
+  dBackground: "md:bg-[url('./assets/beachDesk.jpg')]",
+}
+
+export { cold, hot }
